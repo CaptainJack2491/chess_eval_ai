76312d75d755fa8aa151c4a0ae6b609aaa8f0b13
CaptainJack2491 <jayrupnakawala@gmail.com>
2024-03-02 21:56:43 +0000

    converted fen into two lists of int and str
    finally found a way to convert the fen notaion to two lists of strings and integers.
    now i just need to replace the strings (pieces) into numbers (values of those pieces).

--- main.py
+++ main.py
@@ -1,36 +1,75 @@
-import numpy as np
-import pandas as pd
-from pandas import DataFrame
+# import numpy as np
+# import pandas as pd
+# from pandas import DataFrame
 
-data:DataFrame = pd.read_csv("./dataset/random_evals.csv")
+# data:DataFrame = pd.read_csv("./dataset/random_evals.csv")
 
 
-board: list[str] = [
-    "_","_","_","_","_","_","_","_",
-    "_","_","_","_","_","_","_","_",
-    "_","_","_","_","_","_","_","_",
-    "_","_","_","_","_","_","_","_",
-    "_","_","_","_","_","_","_","_",
-    "_","_","_","_","_","_","_","_",
-    "_","_","_","_","_","_","_","_",
-    "_","_","_","_","_","_","_","_",
-]
-
-def fen_to_array(fen: str) -> list[str]:
+def fen_to_array(fen: str) -> str:
     """
-    replaces blank value ("_") in {board} with values in FEN
+    Converts FEN into a string of 64 characters
 
     """
+    count:int = 0
+    
+    final:str = ''
+    # create a list of 64 0s
     for letter in fen:
         # rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR
-        count = 0
-        if type(letter) == int:
-            count += 1
-        elif expression:
+        if letter == '/':
             pass
+        elif letter.isdigit():
+            for _ in range(int(letter)):
+                count += 1
+                final += '0'
+        else:
+                final += letter
+                count += 1
 
 
-    return board
+    return final
 
 
-print(data.FEN[0])
+def split_fen(fen:str) -> list[list]:
+    """
+    splits FEN into two lists of strings 
+    """
+    fen_black:list = []
+    fen_white:list = []
+
+    for i in fen:
+        if i.islower():
+            fen_black.append(i)
+            fen_white.append(0)
+        elif i.isupper():
+            fen_white.append(i)
+            fen_black.append(0)
+        elif i.isdigit():
+            i_int = int(i)
+            fen_white.append(i_int)
+            fen_black.append(i_int)
+        else:
+            fen_white.append(i)
+            fen_black.append(i)
+
+    return [fen_black, fen_white]
+
+
+
+test = fen_to_array("r1bqk2r/pp1n2pp/2n1pp2/3pP3/1b1P1P2/3B1N2/PP1B2PP/R2QK1NR")
+test_split = split_fen(test)
+print(test)
+
+
+
+print("-------------------------")
+
+for i in test_split:
+    print(i)
+    print(len(i))
+    print("******************")
+
+
+
+
+# print(data.FEN[0])
