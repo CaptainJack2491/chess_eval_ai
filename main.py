b79607dd853628744dc3ed5cf92cf4d69fa40fb7
CaptainJack2491 <jayrupnakawala@gmail.com>
2024-03-16 14:57:39 +0000

    converted fen to two list of 64 with values of each piece and 0 for empty

--- main.py
+++ main.py
@@ -7,26 +7,21 @@
 
 def fen_to_array(fen: str) -> str:
     """
-    Converts FEN into a string of 64 characters
-
+    Converts FEN into a string of 64 characters by replacing the numbers by 0s
     """
-    count:int = 0
-    
+
     final:str = ''
-    # create a list of 64 0s
     for letter in fen:
-        # rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR
+        # r1bqk2r/pp1n2pp/2n1pp2/3pP3/1b1P1P2/3B1N2/PP1B2PP/R2QK1NR
         if letter == '/':
             pass
         elif letter.isdigit():
             for _ in range(int(letter)):
-                count += 1
                 final += '0'
         else:
-                final += letter
-                count += 1
+            final += letter
 
-
+    # r0bqk00rpp0n00pp00n0pp00000pP0000b0P0P00000B0N00PP0B00PPR00QK0NR
     return final
 
 
@@ -42,7 +37,7 @@ def split_fen(fen:str) -> list[list]:
             fen_black.append(i)
             fen_white.append(0)
         elif i.isupper():
-            fen_white.append(i)
+            fen_white.append(i.lower())
             fen_black.append(0)
         elif i.isdigit():
             i_int = int(i)
@@ -51,18 +46,18 @@ def split_fen(fen:str) -> list[list]:
         else:
             fen_white.append(i)
             fen_black.append(i)
-
+        
+    print(fen_black)
+    print(fen_white)
     return [fen_black, fen_white]
 
+
+dict = {'p':1,'n':3,'b':3,'r':5,'q':9,'k':20,0:0}
 
 
 test = fen_to_array("r1bqk2r/pp1n2pp/2n1pp2/3pP3/1b1P1P2/3B1N2/PP1B2PP/R2QK1NR")
 test_split = split_fen(test)
 print(test)
-
-
-
-print("-------------------------")
 
 for i in test_split:
     print(i)
@@ -71,5 +66,7 @@ for i in test_split:
 
 
 
+print(list(map(dict.get,test_split[0])))
+print(list(map(dict.get,test_split[1])))
 
 # print(data.FEN[0])
