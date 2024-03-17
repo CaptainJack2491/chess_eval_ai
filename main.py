70c6baccdb6558a175de840079c80f6e82b1928b
CaptainJack2491 <jayrupnakawala@gmail.com>
2024-03-17 17:52:02 +0000

    finished the function to convert fen to number

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # import numpy as np
 # import pandas as pd
 # from pandas import DataFrame
+# import tensorflow as tf
 
 # data:DataFrame = pd.read_csv("./dataset/random_evals.csv")
 
@@ -49,10 +50,13 @@ def split_fen(fen:str) -> list[list]:
         
     print(fen_black)
     print(fen_white)
-    return [fen_black, fen_white]
+    dict = {'p':1,'n':3,'b':3,'r':5,'q':9,'k':20,0:0}
+
+    transformed_black = list(map(dict.get,fen_black))
+    transformed_white = list(map(dict.get,fen_white))
+    return [transformed_black, transformed_white]
 
 
-dict = {'p':1,'n':3,'b':3,'r':5,'q':9,'k':20,0:0}
 
 
 test = fen_to_array("r1bqk2r/pp1n2pp/2n1pp2/3pP3/1b1P1P2/3B1N2/PP1B2PP/R2QK1NR")
@@ -65,8 +69,13 @@ for i in test_split:
     print("******************")
 
 
+print(test_split[0])
+print(test_split[1])
 
-print(list(map(dict.get,test_split[0])))
-print(list(map(dict.get,test_split[1])))
+
+
+
+
+
 
 # print(data.FEN[0])
